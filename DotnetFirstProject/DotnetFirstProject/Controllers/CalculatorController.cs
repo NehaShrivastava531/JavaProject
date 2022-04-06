b1bef22aa281f884fa52b2d568654938e15cd0b4
nehashrivastava <neha.shrivastava@nagarro.com>
2022-04-06 10:11:20 +0530

    add dotnet project

---
+++ DotnetFirstProject/DotnetFirstProject/Controllers/CalculatorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetFirstProject.Controllers
+{
+    public class CalculatorController : Controller
+    {   
+        public string Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return "Addition = "+ result;
+        }
+
+        public string Sub(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return "Subtraction = " + result;
+        }
+
+        public string Mul(int num1, int num2)
+        {
+            int result = num1 * num2;
+
+            return "Multiplication = " + result;
+        }
+
+        public string Div(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return "Division = " + result;
+        }
+    }
+}
